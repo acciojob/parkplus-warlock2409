6ee7860a1dd0b699454f4dc4434a8f3a1718d238
rajaram <rajaram@runloyal.com>
2023-07-02 23:57:07 +0530

    fir[6

--- src/main/java/com/driver/controllers/ReservationController.java
+++ src/main/java/com/driver/controllers/ReservationController.java
@@ -25,6 +25,7 @@ public class ReservationController {
             return reservation;
         }catch (Exception e){
 //            throw new Exception("Cannot make reservation");
+            return null;
         }
 
     }
